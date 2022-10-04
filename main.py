ab5127f6158a9f7dc5642f54647d30c1c0b1b87b
Esuong-Bassey <basseyesuong@gmail.com>
2022-10-05 00:02:43 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 print("Hello World")
 print("Happy Weekend")
 
-// Two extra lines added
-first extra line
-second extra line
-print("extra lines")
+# Two extra lines added
+extraLine = "first extra line"
+extraLines = "second extra line"
+print(extraLines)
